3aea35f9ca029ce80fa2b46d091579ae6802dd0f
Alexander Fehr <afehr35@outlook.com>
2023-02-28 17:19:15 +0100

    Added format options to input

--- SoundRipper/src/InputCard.tsx
+++ SoundRipper/src/InputCard.tsx
@@ -7,13 +7,26 @@ export default function InputCard(){
     return (
         <div className="input-card">
             <div className="input-group">
-                <input type="text" aria-label="First name" className="form-control"/>
+                <input type="text" aria-label="First name" placeholder="Link" className="form-control"/>
 
                 <select className="form-select" aria-label="Default select example">
-                    <option selected>Format</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                    <optgroup label="Audio Interchange File">
+                        <option value="1">.aif - 32k</option>
+                        <option value="2">.aif - 64k</option>
+                        <option selected value="3">.aif - 128k </option>
+                    </optgroup>
+
+                    <optgroup label="MPEG-1 Audio Layer 3">
+                        <option value="1">.mp3 - 32k</option>
+                        <option value="2">.mp3 - 64k</option>
+                        <option value="3">.mp3 - 128k </option>
+                    </optgroup>
+
+                    <optgroup label="Waveform Audio File">
+                        <option value="1">.wav - 32k</option>
+                        <option value="2">.wav - 64k</option>
+                        <option value="3">.wav - 128k </option>
+                    </optgroup>
                 </select>
             </div>
             <div className="download-button">
@@ -21,4 +34,8 @@ export default function InputCard(){
             </div>
         </div>
     )
+}
+
+function handleSubmit(){
+    //Handle form submit
 }
