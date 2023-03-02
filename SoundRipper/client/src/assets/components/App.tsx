15bfe721ae9a733b68342649a536f15fc2eb89ec
Alexander Fehr <afehr35@outlook.com>
2023-03-02 19:41:09 +0100

    Reorg

---
+++ SoundRipper/client/src/assets/components/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import InputCard from './InputCard'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import "../css/InputCard.css"
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <InputCard />
+    </div>
+  )
+}
